aa96c9a5c6ca0e4a01d51bb17fd1e09ac11b12dc
Ashik Adnan <ashikadnan@live.com>
2022-03-05 16:00:06 +0600

    End of section 8

--- API/Startup.cs
+++ API/Startup.cs
@@ -45,7 +45,7 @@ namespace API
             services.AddCors(opt => 
             {
                 opt.AddPolicy("CorsPolicy", policy=> {
-                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:5169");
+                    policy.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200");
                 });
             });
                        
